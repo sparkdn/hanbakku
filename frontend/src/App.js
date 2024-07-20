6857a21f9140e020ba5ba263c237320cea421017
HyeseungNA <nahs00122@gmail.com>
2024-07-20 15:35:55 +0900

    config setting environment

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Main from "./pages/MainPage";
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/MainPage/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Main() {
+  return <div>메인페이지 입니당</div>;
+}
