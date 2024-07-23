300ed9e2a4c7173dcbdf188b680da04a2b9e5db9
Hyunho Lee <lhh9799@gmail.com>
2024-07-23 21:00:44 +0900

    config: New Backend Project

---
+++ backend/src/main/java/com/spark/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.spark.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		//임의 수정
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
