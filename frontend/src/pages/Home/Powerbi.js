74fde0cfe7973e8f5e02d9ab008e886f1fd5b043
Ji <26162616@naver.com>
2024-08-12 20:53:46 +0900

    powerbi temp

--- frontend/src/pages/Home/Powerbi.js
+++ frontend/src/pages/Home/Powerbi.js
@@ -1,13 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { PowerBIEmbed } from 'powerbi-client-react';
 import { models } from 'powerbi-client';
 import styles from "./powerbi.css";
+import { PublicClientApplication } from '@azure/msal-browser';
+
 
 function PowerBI() {
+    // // 1. azure 사용자 인증, 사용자 token 받아오기
+    // const [accessToken, setAcsToken] = useState("");
+    // const msalConfig = {
+    //     auth: {
+    //         clientId: process.env.REACT_APP_AZ_CLIENT_ID,
+    //         authority: `https://login.microsoftonline.com/${process.env.REACT_APP_AZ_TENANT_ID}`,
+    //         redirectUri: process.env.REACT_APP_AZ_REDIRECT_URI,
+    //     },
+    //     cache: {
+    //         cacheLocation: 'sessionStorage',
+    //         storeAuthStateInCookie: false,
+    //     },
+    // };localStorage
 
+    // const pca = new PublicClientApplication(msalConfig);
+
+    // // 1-2. 로그인 요청
+    // useEffect(() => {
+    //     // 비동기 함수 정의 및 호출
+    //     const handleAuth = async () => {
+    //         await pca.initialize();
+    //         try {
+    //             // 리디렉션 후 응답 처리
+    //             const response = await pca.handleRedirectPromise();
+
+    //             if (response) {
+    //                 // 리디렉션 후 응답 처리
+    //                 setAcsToken(response.accessToken);
+    //             } else {
+    //                 // 리디렉션이 없는 경우 로그인 프로세스 시작
+    //                 const loginRequest = {
+    //                     scopes: ["https://api.powerbi.com/.default"]
+    //                 };
+    //                 await pca.loginRedirect(loginRequest);
+    //             }
+    //         } catch (error) {
+    //             console.error('리디렉션 처리 오류:', error);
+    //         }
+    //     };
+
+    //     handleAuth();
+    // }, [pca]);
+
+    //2. powerbi 보고서 token 발급
+    const [myPBtoken, setPBToken] = useState("");
+    const mypowerbigoupid = process.env.REACT_APP_MY_POWERBI_GROUP_ID;
     const mypowerbiid = process.env.REACT_APP_MY_POWERBI_ID;
     const mypowerbiul = process.env.REACT_APP_MY_POWERBI_ID_URL;
     const mypowerbitoken = process.env.REACT_APP_MY_POWERBI_TOKEN;
+    const uri = "https://api.powerbi.com/v1.0/myorg/groups/" + mypowerbigoupid + "/reports/" + mypowerbiid + "/GenerateToken"
+
+    // useEffect(() => {
+    //     fetch(uri, {
+    //         method: "POST",
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             'Authorization': mypowerbitoken,//`Bearer ${accessToken}`,
+    //         },
+    //         body: {
+    //             "accessLevel": "View",
+    //             "allowSaveAs": "false"
+    //         },
+    //     })
+    //         .then(res => {
+    //             console.log(res);
+    //             res.json();
+    //         }).then(res => setPBToken(res.token));
+
+    // }, []);
 
     return (
         <div className="box">
@@ -16,8 +83,8 @@ function PowerBI() {
                     {
                         type: 'report', // Supported types: report, dashboard, tile, visual, and qna.
                         id: mypowerbiid,
-                        embedUrl:mypowerbiul,
-                        accessToken: mypowerbitoken,
+                        embedUrl: mypowerbiul,
+                        accessToken: myPBtoken,
                         tokenType: models.TokenType.Embed, // Use models.TokenType.Aad if you're embedding for your organization.
                         settings: {
                             panes: {
